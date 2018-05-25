9da759e87ade8264f0472a3752fd5e66f30237d5
Chris Larsen <clarsen@instructure.com>
2018-05-25 11:28:49 -0600

    chore: Initial Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='terraflow',
+    version='0.1.0',
+    author='Chris Larsen',
+    author_email='clarsen801@gmail.com',
+    packages=find_packages()
+)
